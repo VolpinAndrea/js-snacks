20b05d9dc5ccae296b2f5279a850b9569232b48c
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-14 16:43:46 +0100

    BONUS Ex6

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,6 +1,12 @@
 
-let random = Math.round(Math.random()*10);
-let numUtente = prompt("Inserisci un numero da 1 a 10");    
+let max = prompt("Inesrisci il numero massimo consentito")
+
+let random = Math.round(Math.random()*(max-1))+1;
+
+let numUtente = prompt("Inserisci un numero da 1 a "+ max);    
+
+
+console.log("TU "+numUtente+":"+random+" COMPUTER");
 
 if (numUtente == random){
     console.log("Hai vinto!")
@@ -8,5 +14,4 @@ if (numUtente == random){
     console.log("Hai perso")
 }
 
-console.log(numUtente+":"+random);
 
