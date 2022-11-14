0f9bf984fb98d2bb22be641b3e600e00abb76da8
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-14 16:05:39 +0100

    FINE

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,13 +1,4 @@
 
-/* let parola1 = prompt("Inserisci una parola");
-let parola2 = prompt("Inserisci un'altra parola");
-
-if (parola1.length < parola2.length){
-    console.log(parola1 + " " + parola2);
-} else {
-    console.log(parola2 + " " + parola1);
-
-} */
 let somma = 0;
 
 for (let i = 0; i < 10; i++) {
--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,16 +1,4 @@
 
-/* let dispari = [];
-
-for (let i = 0; i < 6; i++) {
-    let numero = prompt("Inserisci un numero " + (i+1));    
-
-    if(numero%2 != 0){
-        dispari.push(numero);
-    }
-}
-// volevo farlo con un ciclo for ma funziona anche così
-console.log(dispari);
-*/ 
 let random = Math.round(Math.random()*10);
 let numUtente = prompt("Inserisci un numero da 1 a 10");    
 
