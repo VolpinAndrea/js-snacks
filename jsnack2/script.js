3a4b3cdb94c17a0439419ed721fd365ba1259973
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-14 15:00:41 +0100

    Ex2

---
+++ jsnack2/script.js
@@ -0,0 +1,13 @@
+
+let parola1 = prompt("Inserisci una parola");
+let parola2 = prompt("Inserisci un'altra parola");
+
+if (parola1.length < parola2.length){
+    console.log(parola1 + " " + parola2);
+} else {
+    console.log(parola2 + " " + parola1);
+
+}
+
+
+
