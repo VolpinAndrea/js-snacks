cb03e8c418711c14a5a6962cca2b209dd7a3f14a
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-14 16:29:54 +0100

    BONUS ex3-4

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,12 +1,29 @@
 
 let somma = 0;
+let maggiore = 0;
+let minore = 0;
 
 for (let i = 0; i < 10; i++) {
     let numero = prompt("Inserisci un numero " + (i+1));
     somma += parseInt(numero); 
     //somma = somma+numero;
-   // console.log(somma);
+    //console.log(somma);
+
+    if(i == 0){
+        maggiore = numero;
+    }else if (i>=1 && numero >= maggiore){
+        maggiore = numero;
+    }
+    if(i == 0){
+        minore = numero;
+    }else if (i>=1 && numero <= minore){
+        minore = numero;
+    }
+
 }
 
 
-console.log(somma);
+console.log("Somma: "+somma);
+console.log("Media: "+ somma/10)
+console.log("Maggiore: "+ maggiore)
+console.log("minore: "+ minore)
