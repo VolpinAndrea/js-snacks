d05cde6a1ea6d2bb1a7a9773bb372b5891297e74
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-14 14:55:39 +0100

    Ex1

---
+++ jsnack1/script.js
@@ -0,0 +1,13 @@
+
+let num1 = prompt("Inserisci il primo numero");
+let num2 = prompt("Inserisci il secondo numero");
+
+if (num1 < num2){
+    console.log("Il primo numero "+num1+" è minore del secondo "+ num2);
+} else {
+    console.log("Il primo numero "+num1+" è maggiore del secondo "+ num2);
+
+}
+
+
+
